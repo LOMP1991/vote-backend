534ae8b139d75e8ccda52cd267a62de7ed77b64d
OrlandoMP <LUISORLANDOMP@HOTMAIL.COM>
2025-12-10 00:23:30 -0500

    actualizacion modulos

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registrarUser } from '../controllers/userController.js';
+import { registrarUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
