91276ec6027d08f380f5a4cf6072d748434ff865
OrlandoMP <LUISORLANDOMP@HOTMAIL.COM>
2025-12-11 23:38:47 -0500

    Ruta Listar Elecciones

--- routes/electionRoutes.js
+++ routes/electionRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { createElection } from "../controllers/electionController.js";
+import { createElection, getElections } from "../controllers/electionController.js";
+
 
 const router = express.Router();
 
-router.post('/create', createElection);
+router.post("/create", createElection);
+router.get("/list", getElections);
+
 
 export default router;
